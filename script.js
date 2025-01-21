a42f64d3236c75c767a383d5f731c34f4bbcf3a3
Koushor <koushor2004@gmail.com>
2025-01-22 02:47:17 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let ptr=document.querySelector(".box1")
+
+ptr.addEventListener("dblclick",()=>{
+    let count=0;
+    let count2=0;
+    document.querySelector(".box1").innerHTML="";
+    ptr.addEventListener("click",()=>{
+        count++;
+        document.getElementById("clicks").innerHTML=count;
+        // document.getElementById("cps").innerHTML=count/5;
+    })
+
+    setInterval(() => {
+        count2+=0.5;
+        document.getElementById("cps").innerHTML=(count/count2);
+        // count2+=0.1;
+    }, 500);
+    setTimeout(() => {
+        
+        alert(`Your CPS is ${count/5}`);
+
+        count=0;
+        document.getElementById("clicks").innerHTML=0;
+        document.getElementById("cps").innerHTML=0;
+        count2=0;
+    }, 5000);
+}
+)
+
